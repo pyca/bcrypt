c4b252b53f941028bda0261c56e942a9fe566b9f
Donald Stufft <donald@stufft.io>
2013-05-19 08:42:34 -0400

    Don't attempt to load assembler on i386

--- bcrypt/__init__.py
+++ bcrypt/__init__.py
@@ -56,6 +56,8 @@ _bcrypt_lib = _ffi.verify('#include "ow-crypt.h"',
         str(os.path.join(_bundled_dir, "crypt_gensalt.c")),
         str(os.path.join(_bundled_dir, "wrapper.c")),
         # How can we get distutils to work with a .S file?
+        #   Set https://github.com/dstufft/bcrypt/blob/4c939e895bd9607301cda6d6f05ef3c1146eb658/bcrypt/crypt_blowfish-1.2/crypt_blowfish.c#L57
+        #      back to 1 if we get ASM loaded.
         # str(os.path.join(_bundled_dir, "x86.S")),
     ],
     include_dirs=[str(_bundled_dir)],
