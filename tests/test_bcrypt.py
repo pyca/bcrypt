ea678c1dd1260495cb9ac51caebd4d884ea3098b
Alex Gaynor <alex.gaynor@gmail.com>
2015-03-13 21:28:16 -0400

    Error out on NUL bytes.

--- tests/test_bcrypt.py
+++ tests/test_bcrypt.py
@@ -264,3 +264,7 @@ def test_hashpw_str_salt():
             b"password",
             six.text_type("$2a$04$cVWp4XaNU8a4v1uMRum2SO"),
         )
+
+def test_nul_byte():
+    with pytest.raises(ValueError):
+        bcrypt.hashpw(b"abc\0def", bcrypt.gensalt(0))
