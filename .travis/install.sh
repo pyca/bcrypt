ee916a825cf79c148918577aa61fda2078c6d630
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-06-22 10:17:36 -0500

    add OS X (no wait, mac OS) to travis configs

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,13 +3,36 @@
 set -e
 set -x
 
-if [[ "${TOXENV}" == "pypy" ]]; then
+install_pypy () {
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
     PYENV_ROOT="$HOME/.pyenv"
     PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
-    pyenv install pypy-2.6.0
-    pyenv global pypy-2.6.0
+}
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    install_pypy
+    case "${TOXENV}" in
+        py27)
+            curl -O https://bootstrap.pypa.io/get-pip.py
+            python get-pip.py --user
+            ;;
+        py35)
+            pyenv install 3.5.1
+            pyenv global 3.5.1
+            ;;
+    esac
+    pyenv rehash
+    python -m pip install --user virtualenv
+else
+    if [[ "${TOXENV}" == "pypy" ]]; then
+        install_pypy
+        pyenv install pypy-2.6.0
+        pyenv global pypy-2.6.0
+    fi
+    pip install virtualenv
 fi
 
+python -m virtualenv ~/.venv
+source ~/.venv/bin/activate
 pip install tox
