74e6a256ad5269bcd69739669e92ee5ba2da7644
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-06-28 18:59:42 -0500

    clean up some warnings

--- src/_csrc/pycabcrypt.h
+++ src/_csrc/pycabcrypt.h
@@ -30,3 +30,4 @@ typedef uint64_t u_int64_t;
 int bcrypt_hashpass(const char *key, const char *salt, char *encrypted, size_t encryptedlen);
 int encode_base64(char *, const u_int8_t *, size_t);
 int timingsafe_bcmp(const void *b1, const void *b2, size_t n);
+int bcrypt_pbkdf(const char *pass, size_t passlen, const uint8_t *salt, size_t saltlen, uint8_t *key, size_t keylen, unsigned int rounds);
