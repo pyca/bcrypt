717fe63db6945f65a5d71930d6fb42110bcf0229
Alex Gaynor <alex.gaynor@gmail.com>
2017-01-02 00:08:49 -0500

    We do the py36 (#102)

--- setup.py
+++ setup.py
@@ -229,6 +229,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
 
     ext_package="bcrypt",
