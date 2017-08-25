b97b064048c43b9bb21ed2ace5c533702afe6fa0
Alex Gaynor <alex.gaynor@gmail.com>
2017-08-25 10:09:30 -0400

    Fixed #122 -- added checkpw to __all__ so it shows up in docs

--- src/bcrypt/__init__.py
+++ src/bcrypt/__init__.py
@@ -33,7 +33,7 @@ from .__about__ import (
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
     "__email__", "__license__", "__copyright__",
-    "gensalt", "hashpw", "kdf",
+    "gensalt", "hashpw", "kdf", "checkpw",
 ]
 
 
