3c3a2cec35507ad3e90f5eb0fb76e54e847c317f
Alex Gaynor <alex.gaynor@gmail.com>
2017-09-04 10:07:01 -0400

    Fix travis build (#126)
    * Fix travis build
    
    * This isn't required anymore

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,15 +3,7 @@
 set -e
 set -x
 
-install_pyenv () {
-    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-    PYENV_ROOT="$HOME/.pyenv"
-    PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-}
-
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    install_pyenv
     case "${TOXENV}" in
         py27)
             curl -O https://bootstrap.pypa.io/get-pip.py
@@ -25,11 +17,6 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv rehash
     python -m pip install --user virtualenv
 else
-    if [[ "${TOXENV}" == "pypy" ]]; then
-        install_pyenv
-        pyenv install pypy-2.6.0
-        pyenv global pypy-2.6.0
-    fi
     pip install virtualenv
 fi
 
