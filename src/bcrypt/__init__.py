017adc83e39dc3026c9e947eee18ada79e3ea41b
David Manthey <manthey@orbitals.com>
2018-07-02 22:36:04 -0400

    Use a relative import.
    Use an application relative import when importing _bcrypt.  This allows
    the package to work even in odd locations, such as
    `pip install --target . bcrypt` (rather than in the site-packages or
    user packages directory).

--- src/bcrypt/__init__.py
+++ src/bcrypt/__init__.py
@@ -22,8 +22,7 @@ import warnings
 
 import six
 
-from bcrypt import _bcrypt
-
+from . import _bcrypt
 from .__about__ import (
     __author__, __copyright__, __email__, __license__, __summary__, __title__,
     __uri__, __version__,
