3eb04d03be0065acb12222fa6ed0a7a02b00dd20
Alex Gaynor <alex.gaynor@gmail.com>
2019-05-26 11:29:40 -0400

    Remove darwin from install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,22 +3,7 @@
 set -e
 set -x
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    case "${TOXENV}" in
-        py27)
-            curl -O https://bootstrap.pypa.io/get-pip.py
-            python get-pip.py --user
-            ;;
-        py35)
-            pyenv install 3.5.1
-            pyenv global 3.5.1
-            ;;
-    esac
-    pyenv rehash
-    python -m pip install --user virtualenv
-else
-    pip install virtualenv
-fi
+pip install virtualenv
 
 python -m virtualenv ~/.venv
 source ~/.venv/bin/activate
