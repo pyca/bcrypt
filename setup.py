92fd05271fa95b0f118866ccd374cd355fcc1baf
Alex Gaynor <alex.gaynor@gmail.com>
2019-10-19 23:20:40 -0400

    Fixes #187 -- test and build wheels on py38 (#188)
    * Fixes #187 -- test and build wheels on py38
    
    * Remove 3.8 for now
    
    * Tell tox to use isolated builds

--- setup.py
+++ setup.py
@@ -233,6 +233,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 
     ext_package="bcrypt",
