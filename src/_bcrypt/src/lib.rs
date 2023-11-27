55e8e2f1a17323ae79ab35addabffbb96f3b7c37
Alex Gaynor <alex.gaynor@gmail.com>
2023-11-27 11:46:09 -0500

    Use a different version attribute name

--- src/_bcrypt/src/lib.rs
+++ src/_bcrypt/src/lib.rs
@@ -183,7 +183,9 @@ fn _bcrypt(_py: pyo3::Python<'_>, m: &pyo3::types::PyModule) -> pyo3::PyResult<(
     m.add("__uri__", "https://github.com/pyca/bcrypt/")?;
 
     // When updating this, also update pyproject.toml
-    m.add("__version__", "4.1.1")?;
+    // This isn't named __version__ because passlib treats the existence of
+    // that attribute as proof that we're a different module
+    m.add("__version_ex__", "4.1.1")?;
 
     let author = "The Python Cryptographic Authority developers";
     m.add("__author__", author)?;
