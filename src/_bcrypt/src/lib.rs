e92e63d3b132382ea083f4c432fcc4a78dc62cdd
Alex Gaynor <alex.gaynor@gmail.com>
2024-03-25 21:27:54 -0400

    Upgrade to pyo3 0.21

--- src/_bcrypt/src/lib.rs
+++ src/_bcrypt/src/lib.rs
@@ -13,6 +13,7 @@
 #![deny(rust_2018_idioms)]
 
 use base64::Engine;
+use pyo3::prelude::PyBytesMethods;
 use pyo3::PyTypeInfo;
 use std::convert::TryInto;
 use std::io::Write;
@@ -28,7 +29,7 @@ fn gensalt<'p>(
     py: pyo3::Python<'p>,
     rounds: Option<u16>,
     prefix: Option<&[u8]>,
-) -> pyo3::PyResult<&'p pyo3::types::PyBytes> {
+) -> pyo3::PyResult<pyo3::Bound<'p, pyo3::types::PyBytes>> {
     let rounds = rounds.unwrap_or(12);
     let prefix = prefix.unwrap_or(b"2b");
 
@@ -47,7 +48,7 @@ fn gensalt<'p>(
 
     let encoded_salt = BASE64_ENGINE.encode(salt);
 
-    pyo3::types::PyBytes::new_with(
+    pyo3::types::PyBytes::new_bound_with(
         py,
         1 + prefix.len() + 1 + 2 + 1 + encoded_salt.len(),
         |mut b| {
@@ -68,7 +69,7 @@ fn hashpw<'p>(
     py: pyo3::Python<'p>,
     password: &[u8],
     salt: &[u8],
-) -> pyo3::PyResult<&'p pyo3::types::PyBytes> {
+) -> pyo3::PyResult<pyo3::Bound<'p, pyo3::types::PyBytes>> {
     // bcrypt originally suffered from a wraparound bug:
     // http://www.openwall.com/lists/oss-security/2012/01/02/4
     // This bug was corrected in the OpenBSD source by truncating inputs to 72
@@ -112,18 +113,14 @@ fn hashpw<'p>(
     let hashed = py
         .allow_threads(|| bcrypt::hash_with_salt(password, cost, raw_salt))
         .map_err(|_| pyo3::exceptions::PyValueError::new_err("Invalid salt"))?;
-    Ok(pyo3::types::PyBytes::new(
+    Ok(pyo3::types::PyBytes::new_bound(
         py,
         hashed.format_for_version(version).as_bytes(),
     ))
 }
 
 #[pyo3::prelude::pyfunction]
-fn checkpw(
-    py: pyo3::Python<'_>,
-    password: &[u8],
-    hashed_password: &[u8],
-) -> pyo3::PyResult<bool> {
+fn checkpw(py: pyo3::Python<'_>, password: &[u8], hashed_password: &[u8]) -> pyo3::PyResult<bool> {
     Ok(hashpw(py, password, hashed_password)?
         .as_bytes()
         .ct_eq(hashed_password)
@@ -138,7 +135,7 @@ fn kdf<'p>(
     desired_key_bytes: usize,
     rounds: u32,
     ignore_few_rounds: Option<bool>,
-) -> pyo3::PyResult<&'p pyo3::types::PyBytes> {
+) -> pyo3::PyResult<pyo3::Bound<'p, pyo3::types::PyBytes>> {
     let ignore_few_rounds = ignore_few_rounds.unwrap_or(false);
 
     if password.is_empty() || salt.is_empty() {
@@ -163,15 +160,15 @@ fn kdf<'p>(
         // They probably think bcrypt.kdf()'s rounds parameter is logarithmic,
         // expecting this value to be slow enough (it probably would be if this
         // were bcrypt). Emit a warning.
-        pyo3::PyErr::warn(
+        pyo3::PyErr::warn_bound(
             py,
-            pyo3::exceptions::PyUserWarning::type_object(py),
+            &pyo3::exceptions::PyUserWarning::type_object_bound(py),
             &format!("Warning: bcrypt.kdf() called with only {rounds} round(s). This few is not secure: the parameter is linear, like PBKDF2."),
             3
         )?;
     }
 
-    pyo3::types::PyBytes::new_with(py, desired_key_bytes, |output| {
+    pyo3::types::PyBytes::new_bound_with(py, desired_key_bytes, |output| {
         py.allow_threads(|| {
             bcrypt_pbkdf::bcrypt_pbkdf(password, salt, rounds, output).unwrap();
         });
@@ -180,14 +177,20 @@ fn kdf<'p>(
 }
 
 #[pyo3::prelude::pymodule]
-fn _bcrypt(_py: pyo3::Python<'_>, m: &pyo3::types::PyModule) -> pyo3::PyResult<()> {
+fn _bcrypt(
+    _py: pyo3::Python<'_>,
+    m: &pyo3::Bound<'_, pyo3::types::PyModule>,
+) -> pyo3::PyResult<()> {
     m.add_function(pyo3::wrap_pyfunction!(gensalt, m)?)?;
     m.add_function(pyo3::wrap_pyfunction!(hashpw, m)?)?;
     m.add_function(pyo3::wrap_pyfunction!(checkpw, m)?)?;
     m.add_function(pyo3::wrap_pyfunction!(kdf, m)?)?;
 
     m.add("__title__", "bcrypt")?;
-    m.add("__summary__", "Modern(-ish) password hashing for your software and your servers")?;
+    m.add(
+        "__summary__",
+        "Modern(-ish) password hashing for your software and your servers",
+    )?;
     m.add("__uri__", "https://github.com/pyca/bcrypt/")?;
 
     // When updating this, also update pyproject.toml
