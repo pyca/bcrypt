bfa50a0d9184fefd1639f21a1cdf614b3501d1be
Alex Gaynor <alex.gaynor@gmail.com>
2024-07-22 08:37:57 -0400

    Bump version for 4.2.0 release

--- src/_bcrypt/src/lib.rs
+++ src/_bcrypt/src/lib.rs
@@ -196,7 +196,7 @@ mod _bcrypt {
         // When updating this, also update pyproject.toml
         // This isn't named __version__ because passlib treats the existence of
         // that attribute as proof that we're a different module
-        m.add("__version_ex__", "4.1.3")?;
+        m.add("__version_ex__", "4.2.0")?;
 
         let author = "The Python Cryptographic Authority developers";
         m.add("__author__", author)?;
