884baedb543649b43fc0df4b7428657c35d28bfc
Alex Gaynor <alex.gaynor@gmail.com>
2024-07-22 12:50:37 -0400

    Import improvements from cryptography wheel building and release (#840)
    Upload to PyPI from GHA

--- release.py
+++ release.py
@@ -10,112 +10,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import getpass
-import io
-import json
-import os
 import subprocess
-import time
-import zipfile
 
 import click
-import requests
 
 
 def run(*args, **kwargs):
     print(f"[running] {list(args)}")
     subprocess.check_call(list(args), **kwargs)
-
-
-def wait_for_build_complete_github_actions(session, token, run_url):
-    while True:
-        response = session.get(
-            run_url,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"token {token}",
-            },
-        )
-        response.raise_for_status()
-        if response.json()["conclusion"] is not None:
-            break
-        time.sleep(3)
-
-
-def download_artifacts_github_actions(session, token, run_url):
-    response = session.get(
-        run_url,
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": f"token {token}",
-        },
-    )
-    response.raise_for_status()
-
-    response = session.get(
-        response.json()["artifacts_url"],
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": f"token {token}",
-        },
-    )
-    response.raise_for_status()
-    paths = []
-    for artifact in response.json()["artifacts"]:
-        response = session.get(
-            artifact["archive_download_url"],
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"token {token}",
-            },
-        )
-        with zipfile.ZipFile(io.BytesIO(response.content)) as z:
-            for name in z.namelist():
-                if not name.endswith((".whl", ".tar.gz")):
-                    continue
-                p = z.open(name)
-                out_path = os.path.join(
-                    os.path.dirname(__file__),
-                    "dist",
-                    os.path.basename(name),
-                )
-                with open(out_path, "wb") as f:
-                    f.write(p.read())
-                paths.append(out_path)
-    return paths
-
-
-def build_github_actions_sdist_wheels(token, version):
-    session = requests.Session()
-
-    response = session.post(
-        "https://api.github.com/repos/pyca/bcrypt/actions/workflows/"
-        "wheel-builder.yml/dispatches",
-        headers={
-            "Content-Type": "application/json",
-            "Accept": "application/vnd.github.v3+json",
-            "Authorization": f"token {token}",
-        },
-        data=json.dumps({"ref": "main", "inputs": {"version": version}}),
-    )
-    response.raise_for_status()
-
-    # Give it a few seconds for the run to kick off.
-    time.sleep(5)
-    response = session.get(
-        (
-            "https://api.github.com/repos/pyca/bcrypt/actions/workflows/"
-            "wheel-builder.yml/runs?event=workflow_dispatch"
-        ),
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": f"token {token}",
-        },
-    )
-    response.raise_for_status()
-    run_url = response.json()["workflow_runs"][0]["url"]
-    wait_for_build_complete_github_actions(session, token, run_url)
-    return download_artifacts_github_actions(session, token, run_url)
 
 
 @click.command()
@@ -124,16 +26,8 @@ def release(version):
     """
     ``version`` should be a string like '0.4' or '1.0'.
     """
-    github_token = getpass.getpass("Github person access token: ")
-
     run("git", "tag", "-s", version, "-m", f"{version} release")
-    run("git", "push", "--tags")
-
-    github_actions_paths = build_github_actions_sdist_wheels(
-        github_token, version
-    )
-
-    run("twine", "upload", *github_actions_paths)
+    run("git", "push", "--tags", "git@github.com:pyca/bcrypt.git")
 
 
 if __name__ == "__main__":
