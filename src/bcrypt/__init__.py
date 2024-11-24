d02ff72df1b4d74875ba3217dc5e959e9525c2f9
Alex Gaynor <alex.gaynor@gmail.com>
2024-11-24 10:55:33 -0500

    Fix new ruff warning (#923)

--- src/bcrypt/__init__.py
+++ src/bcrypt/__init__.py
@@ -28,16 +28,16 @@ from ._bcrypt import (
 )
 
 __all__ = [
-    "gensalt",
-    "hashpw",
-    "checkpw",
-    "kdf",
-    "__title__",
-    "__summary__",
-    "__uri__",
-    "__version__",
     "__author__",
+    "__copyright__",
     "__email__",
     "__license__",
-    "__copyright__",
+    "__summary__",
+    "__title__",
+    "__uri__",
+    "__version__",
+    "checkpw",
+    "gensalt",
+    "hashpw",
+    "kdf",
 ]
