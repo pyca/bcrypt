27a8581b25ae559fd1a3da2caf9d0e4b6f6ec288
Manuel Stausberg <stausberg@denic.de>
2025-03-11 10:36:35 +0100

    update test_2a_wraparound_bug

--- tests/test_bcrypt.py
+++ tests/test_bcrypt.py
@@ -517,12 +517,8 @@ def test_invalid_params(password, salt, desired_key_bytes, rounds, error):
 
 
 def test_2a_wraparound_bug():
-    assert (
-        bcrypt.hashpw(
-            (b"0123456789" * 26)[:255], b"$2a$04$R1lJ2gkNaoPGdafE.H.16."
-        )
-        == b"$2a$04$R1lJ2gkNaoPGdafE.H.16.1MKHPvmKwryeulRe225LKProWYwt9Oi"
-    )
+    with pytest.raises(ValueError):
+        bcrypt.hashpw((b"0123456789" * 26)[:255], b"$2a$04$R1lJ2gkNaoPGdafE.H.16.")
 
 
 def test_multithreading():
